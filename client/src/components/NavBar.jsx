a1785aa5b4ec7f8ae33910f3b5c865722ea9ce5a
Sujan <077bct087.sujan@pcampus.edu.np>
2024-07-15 17:45:12 +0545

    Add navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import {
+  PaperAirplaneIcon,
+} from "@heroicons/react/24/outline";
 
 function NavBar() {
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    navigate('/login'); // Redirect to login page after logout
+  };
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className='text-5xl'>NavBar</div>
-  )
+    <div className='bg-slate-200'>
+      <div className='flex flex-row items-center justify-between p-4'>
+        <div className='text-lg font-bold ml-8 flex'>
+          <PaperAirplaneIcon className="h-7 w-7 text-black mr-5" />
+          <div>
+            Dream Weaver
+          </div>
+        </div>
+        <div className='md:hidden'>
+          <button onClick={toggleMenu} className='text-gray-700 focus:outline-none'>
+            <svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+              <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}></path>
+            </svg>
+          </button>
+        </div>
+        <div className={`hidden md:flex md:flex-row md:items-center ${isOpen ? 'flex flex-col w-full' : ''}`}>
+          <Link to="/" className='block p-4 text-gray-700 '>Home</Link>
+          <Link to="/about" className='block p-4 text-gray-700 '>About</Link>
+          <Link to="/contact-us" className='block p-4 text-gray-700 '>Contact Us</Link>
+          <div className='button cursor-pointer'>Get Started</div>          
+        </div>
+      </div>
+      {isOpen && (
+        <div className='md:hidden'>
+          <Link to="/" className='block p-4 text-gray-700 hover:bg-gray-300'>Home</Link>
+          <Link to="/about" className='block p-4 text-gray-700 hover:bg-gray-300'>About</Link>
+          <Link to="/contact-us" className='block p-4 text-gray-700 hover:bg-gray-300'>Contact</Link>
+
+          <Link to="/interpret-dream" className='block p-4 text-gray-700 hover:bg-gray-300'>Interpret Dream</Link>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
